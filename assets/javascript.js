59ab8ace873b6ffd5b2b79eaef45c4cd9bb9fc1f
Alicia Tamboli <atamboli4718@gmail.com>
2019-05-25 20:51:19 -0400

    button for the characters now working, started ajax call

--- assets/javascript.js
+++ assets/javascript.js
@@ -17,22 +17,40 @@ Bootstrap information for the button I want to use:
 <button type="button" class="btn btn-dark giphyButtons">Dark</button>
 Giphy Api Key:
 8PrL8x52bnoL10zRpUHUhm8xCAXYQCef
+Giphy link to play with:
 
 */
+
+//array to hold the inputs from the user
+var topics=[];
+
+//function to create the button with the character input
+// function buttonCreation () {
+
+//     for(var i=0; i<topics.length; i++) {
+//         console.log(topics[i])
+//     $("#entries").html("<button type='button' class='btn btn-dark giphyButtons' id='"+ topics[i] + "'>" + topics[i] +"</button>");
+//     }
+// }
 
 //click event for #submit button
 $("#submit").on("click",function(){
     //var for the character input from user
-    var character = $("#character").val();
-    console.log(character);
-    //function to create the button with the character input
-    $("#entries").html("<button type='button' class='btn btn-dark giphyButtons' id='"+ character + "'>" + character +"</button>");
+    var character = $("#character").val().trim();
+    topics.push(character);
+    console.log("topics: ");
+    console.log(topics);
+    $("#entries").append("<button type='button' class='btn btn-dark giphyButtons m-1' id='"+ character + "'>" + character +"</button>");
 });
 
+//USE PREPEND
 $(".giphyButton").on("click",function(){
+    var characterButton = $(this);
     $.ajax({
         type:'GET',
-        url:''
+        url:'http://api.giphy.com/v1/gifs/search?q=' + characterButton.val().trim() + '&api_key=8PrL8x52bnoL10zRpUHUhm8xCAXYQCef',
+    }).then(function(response){
+        console.log(response);
     })
 
 });
